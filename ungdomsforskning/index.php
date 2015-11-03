96d553b030d5b915cc91544b472309ffc829b6b6
Eirik Hanssen <eirikhanssen79@gmail.com>
2015-11-03 10:15:11 +0100

    fork nbf theme to ungdomsforskning theme

---
+++ ungdomsforskning/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_nbf
+ */
+
+/**
+ * @file plugins/themes/nbf/index.php
+ *
+ * Copyright (c) 2013-2014 Simon Fraser University Library
+ * Copyright (c) 2003-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_nbf
+ *
+ * @brief Wrapper for "NBF" theme plugin.
+ */
+
+require_once('nbfThemePlugin.inc.php');
+
+return new nbfThemePlugin();
+
+?>
